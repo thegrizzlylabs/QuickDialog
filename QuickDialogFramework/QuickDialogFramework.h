0682fb0d6173b53fbf5cec15984048cb70aeeb3c
Bruno Virlet <bruno.virlet@gmail.com>
2018-07-26 15:09:04 +0200

    Support Carthage by creating a framework

---
+++ QuickDialogFramework/QuickDialogFramework.h
@@ -0,0 +1,18 @@
+//
+//  QuickDialogFramework.h
+//  QuickDialogFramework
+//
+//  Created by Bruno Virlet on 7/26/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuickDialogFramework.
+FOUNDATION_EXPORT double QuickDialogFrameworkVersionNumber;
+
+//! Project version string for QuickDialogFramework.
+FOUNDATION_EXPORT const unsigned char QuickDialogFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickDialogFramework/PublicHeader.h>
+
+#import <QuickDialogFramework/QuickDialog.h>
